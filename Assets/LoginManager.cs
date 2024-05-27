4bbfd73f38b7cd66f5bb0726869752a99f6051db
MAYANS MATÍAS <50926612@est.ort.edu.ar>
2024-05-27 15:51:57 -0300

    Check Password

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+    public GameObject boton;
+    public Text HolderContraseña;
+    public string contraseña;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,16 @@ public class LoginManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Login()
+    {
+        if (HolderContraseña.text==contraseña)
+        {
+            Debug.Log("Acces Granted");
+        }
+        else
+        {
+            Debug.Log("Acces Denied");
+        }
     }
 }
